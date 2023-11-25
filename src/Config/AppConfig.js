c99e1b16b5feb692cabf27a8f28dd004fdc7b821
kamcod <mkamran4786@gmail.com>
2023-11-25 20:50:21 +0500

    - change base url

--- src/Config/AppConfig.js
+++ src/Config/AppConfig.js
@@ -1,5 +1,6 @@
 const AppConfig = {
-    BASE_URL: 'http://146.190.55.109:5000'
+    BASE_URL: 'https://recommendation.decorist.ai:5000'
+    // BASE_URL: 'http://146.190.55.109:5000'
 };
 
 export default AppConfig;
